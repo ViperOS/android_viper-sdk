f038a27f13fc00d9e7b617a33fae9e7e816add84
Pranav Vashi <neobuddy89@gmail.com>
2018-01-22 09:28:39 +0530

    lineage-sdk: Enable advanced reboot by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdk/src/java/org/lineageos/internal/util/PowerMenuUtils.java
+++ sdk/src/java/org/lineageos/internal/util/PowerMenuUtils.java
@@ -27,7 +27,7 @@ public final class PowerMenuUtils {
         KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         boolean keyguardLocked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
         boolean advancedRestartEnabled = LineageSettings.Secure.getInt(context.getContentResolver(),
-                LineageSettings.Secure.ADVANCED_REBOOT, 0) == 1;
+                LineageSettings.Secure.ADVANCED_REBOOT, 1) == 1;
         boolean isPrimaryUser = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
 
         return advancedRestartEnabled && !keyguardLocked && isPrimaryUser;
