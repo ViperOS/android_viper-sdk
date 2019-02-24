LineageOS/android_lineage-sdk

sdk: Cleanup usage of List.toArray(T[] a)
* The parameter here only has to have the type you want it to return,
  so creating an array of size > 0 is unnecessary and wasteful

Change-Id: I28e490fb6fa3703d7edca21b29d640105072947b

lineage/lib/main/java/org/lineageos/platform/internal/LineageWeatherManagerService.java
@@ -375,10 +375,9 @@ public class LineageWeatherManagerService extends LineageSystemService {
             contentValuesList.add(contentValues);
         }
 
-        ContentValues[] updateValues = new ContentValues[contentValuesList.size()];
         if (size != getContext().getContentResolver().bulkInsert(
                 WeatherColumns.CURRENT_AND_FORECAST_WEATHER_URI,
-                contentValuesList.toArray(updateValues))) {
+                contentValuesList.toArray(new ContentValue[0]))) {
             Slog.w(TAG, "Failed to update the weather content provider");
             return false;
         }

ViperOS/android_viper-sdk

lineage/lib/main/java/org/lineageos/platform/internal/LineageWeatherManagerService.java
@@ -375,10 +375,9 @@ public class LineageWeatherManagerService extends LineageSystemService {
             contentValuesList.add(contentValues);
         }
 
-        ContentValues[] updateValues = new ContentValues[contentValuesList.size()];
         if (size != getContext().getContentResolver().bulkInsert(
                 WeatherColumns.CURRENT_AND_FORECAST_WEATHER_URI,
-                contentValuesList.toArray(updateValues))) {
+                contentValuesList.toArray(new ContentValues[0]))) {
             Slog.w(TAG, "Failed to update the weather content provider");
             return false;
         }